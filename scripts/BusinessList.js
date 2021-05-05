9bfee30e5e1284c86260d900f025154e951a0753
alambrow <alexander.lambrow@gmail.com>
2021-05-05 11:01:33 -0500

    search function now works

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,6 +1,22 @@
 import { dataDisplay } from "./Business.js"
 import { grabData } from "./database.js"
 
+const companySearchResults = document.querySelector(".foundCompanies")
+
+document.querySelector("#companySearch").addEventListener("keypress", keyPressEvent => {
+    if (keyPressEvent.charCode === 13) {
+        const businessList = grabData()
+
+        const enteredText = keyPressEvent.target.value.toLowerCase()
+        const foundBusiness = businessList.find(business => business.companyName.toLowerCase().includes(enteredText))
+
+        companySearchResults.innerHTML = dataDisplay(foundBusiness)
+    }
+}
+)
+
+
+
 const contentTarget = document.querySelector(".database")
 
 export const businessList = () => {
--- scripts/database.js
+++ scripts/database.js
@@ -146,27 +146,27 @@ export const grabData = () => {
   return dataCopy
 }
 
-const searchResult = document.querySelector(".foundCompanies")
+// const searchResult = document.querySelector(".foundCompanies")
 
-const dataArray = grabData()
+// const dataArray = grabData()
 
-document
-  .querySelector("#companySearch")
-  .addEventListener(
-    "keypress",
-    keyPressEvent => {
-      if (keyPressEvent.charCode === 13) {
-        /*
-            When the user presses 'Enter', find the matching business.
+// document
+//   .querySelector("#companySearch")
+//   .addEventListener(
+//     "keypress",
+//     keyPressEvent => {
+//       if (keyPressEvent.charCode === 13) {
+//         /*
+//             When the user presses 'Enter', find the matching business.
 
-            You can use the `.includes()` string method to
-            see if a smaller string is part of a larger string.
+//             You can use the `.includes()` string method to
+//             see if a smaller string is part of a larger string.
 
-            Example: business.companyName.includes(keyPressEvent.target.value)
-        */
-        // TODO
-        const foundBusiness = dataArray.find(business => business.companyName.includes(keyPressEvent.target.value))
+//             Example: business.companyName.includes(keyPressEvent.target.value)
+//         */
+//         // TODO
+//         const foundBusiness = dataArray.find(business => business.companyName.includes(keyPressEvent.target.value))
 
-        searchResult.innerHTML = dataDisplay(foundBusiness)
-      }
-    )
+//         searchResult.innerHTML = dataDisplay(foundBusiness)
+//       }
+//     )
